a69c81fea37b33b987db70dbca210e37e76730f5
Tom Flenniken <tomflenniken@yahoo.com>
2020-10-19 23:47:23 -0700

    Attempting fix for pages routing.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/">
           <Home />
