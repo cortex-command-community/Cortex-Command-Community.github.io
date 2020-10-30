d4917f5ad98c1a0995a65dca64fd82168c4a91fb
garethyr <GarethRickwood@hotmail.com>
2020-10-30 05:09:53 +0000

    Deploying to gh-pages from  @ ff78f34248f79ed59f9f482c189e5205a27fefca 🚀

---
+++ precache-manifest.bbf068331ccde36e5dd0a4fa16d3822f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe438bd494f8eac394dab11e5814c52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f53b57dbdf1920e8b1e5",
+    "url": "/static/css/main.5d797848.chunk.css"
+  },
+  {
+    "revision": "725ad93b405a3fe2a674",
+    "url": "/static/js/2.badd6d64.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/static/js/2.badd6d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53b57dbdf1920e8b1e5",
+    "url": "/static/js/main.f773e78c.chunk.js"
+  },
+  {
+    "revision": "500d0a90d5a1aab6cb6f",
+    "url": "/static/js/runtime-main.3d45e8e5.js"
+  }
+]);
