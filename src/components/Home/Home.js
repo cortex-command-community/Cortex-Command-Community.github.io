6dab1e1fd14994025f61ca8df4635646311c46fe
Joshua <45546025+SirJoshi@users.noreply.github.com>
2020-11-18 01:52:05 -0600

    Replaced the Placeholders with Placeholders
    I replaced the black squares with pictures from the game's tutorial files.
    
    I would not call these changes final, but it will save some time for when someone has new pictures/gifs to go here.
    
    the quest for the pink role continues

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,6 +7,12 @@ import CortexCardContainer from './CortexCardContainer/CortexCardContainer';
 import CortexCardContents from './CortexCardContents/CortexCardContents';
 import MagicArrow from './MagicArrow/MagicArrow';
 
+/*i pray to god (if there is one) that there is a beter way to go about this than just manually importing every images*/
+
+import DummySquadImg from './img/Team.png'
+import JoystickImg from './img/Joystick.png'
+import BuymenuImg from './img/BuyMenu.png'
+
 function Home() {
     return (
         <div style={{ backgroundColor: '#111'}}>
@@ -27,19 +33,21 @@ function Home() {
                         <CortexCardContents
                             title='Upgraded Engine'
                             body='The source code has been enhanced to improve performance.'
+                            image={DummySquadImg}
                         />
                     </CortexCard>
                     <CortexCard right>
                         <CortexCardContents
                             right
                             title='New Content'
-                            body='New weapons, characters, and scenes.'
+                            body='New weapons, characters, and scenes.' image={BuymenuImg}
                         />
                     </CortexCard>
                     <CortexCard>
                         <CortexCardContents
                             title='Improved Strategic Gameplay'
                             body='The team has focused on accentuating the most satisfying gameplay elements.'
+                            image={JoystickImg}
                         />
                     </CortexCard>
                 </CortexCardContainer>
