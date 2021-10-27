3db672d44a8f70382287e6d47f74fac1c4b5f58e
ᛈᛟᚲᚲᛖᛏᛋ <55299415+Pockets-byte@users.noreply.github.com>
2021-10-27 12:10:17 -0400

    OUR discord

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -19,12 +19,12 @@ function Footer() {
             }}>
                 <ul>
                     <li><h1>Community</h1></li>
+                    <li><a href='https://discord.gg/invite/TSU6StNQUG'>Our Discord</a></li>
                     <li><a href='https://cccp.mod.io/'>CCCP Mod Portal</a></li>
                     <li><a href='http://forums.datarealms.com/'><span>DataRealms Fan Forums</span></a></li>
                 </ul>
                 <ul>
                     <li><h1>Development</h1></li>
-                    <li><a href='https://discord.gg/invite/TSU6StNQUG'>CCCP Development Discord</a></li>
                     <li><a href='https://github.com/cortex-command-community'><span>Github</span></a></li>
                     <li><a href='https://github.com/cortex-command-community/Cortex-Command-Community-Project-Source/issues'><span>Issues</span></a></li>
                     <li><a href='https://github.com/cortex-command-community/Cortex-Command-Community-Project-Data/wiki'><span>Development Plans and Info</span></a></li>
