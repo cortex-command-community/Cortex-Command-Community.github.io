43a35ed23d2ee4d14b57c65c8bdc7ce89872e3e1
Maxim Toropchin <vimevi57@gmail.com>
2024-03-01 02:20:15 +0300

    added gifs

--- src/components/Home/CortexCardContents/CortexCardContents.js
+++ src/components/Home/CortexCardContents/CortexCardContents.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './CortexCardContents.css';
 
-function CortexCardContents({right, title, body}) {
-    return (
-        <div className='card-contents' style={{ 
-            display: 'flex', 
-            flexDirection: right ? 'row-reverse' : 'initial',
-        }}>
-            <div style={{ flex: '1' }}>
-                <div style={{ 
-                    padding: '8px 16px',
-                }}>
-                    <h1>{title}</h1>
-                    <p>{body}</p>
-                </div>
-            </div>
-            {/* This is an image placeholder **Imagine an Image** */}
-            <div className='image-placeholder'/>
-        </div>
-    );
+function CortexCardContents({ right, title, body, file }) {
+	const [imageLoaded, setImageLoaded] = useState(false);
+
+	useEffect(() => {
+		const img = new Image();
+		img.onload = () => {
+			setImageLoaded(true);
+		};
+		img.src = file;
+	}, []);
+
+	return (
+		<div className={`card-contents ${right ? 'card-contents--reverse' : ''}`}>
+			<div style={{ flex: '1' }}>
+				<div
+					style={{
+						padding: '8px 16px',
+					}}
+				>
+					<h1>{title}</h1>
+					<p>{body}</p>
+				</div>
+			</div>
+			{/* This is an image placeholder **Imagine an Image** */}
+			<div className="image-placeholder">
+				{imageLoaded ? (
+					<img src={file} alt="advantages gif" />
+				) : (
+					<div className="loading-container">Loading...</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default CortexCardContents;
--- src/components/Template/Template.js
+++ src/components/Template/Template.js
@@ -6,7 +6,7 @@ function Template(props) {
 	return (
 		<div className="layout-limits">
 			<div className="template-container">
-				<div className='template-content'>{props.children}</div>
+				<div className="template-content">{props.children}</div>
 			</div>
 		</div>
 	);
